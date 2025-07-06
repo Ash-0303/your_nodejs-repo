7f27c4653ce498a41500d3be8f8b583b784748b7
Ash-0303 <ashwinram44@gmail.com>
2025-07-06 11:40:34 +0100

    Update main.js
    changed port from 80 to 3000

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.end('Hello from Node.js on Lightsail');
 });
-server.listen(80, () => console.log('Server running on port 80'));
+server.listen(3000, () => console.log('Server running on port 3000'));
